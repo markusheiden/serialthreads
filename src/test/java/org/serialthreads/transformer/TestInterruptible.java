fe5760a645b22f128ae0f47b728c51bca10d3c10
Markus Heiden <markus@markusheiden.de>
2010-06-27 20:13:08 +0000

    mh: reworked tests

---
+++ src/test/java/org/serialthreads/transformer/TestInterruptible.java
@@ -0,0 +1,117 @@
+package org.serialthreads.transformer;
+
+import org.serialthreads.Interruptible;
+import org.serialthreads.context.IRunnable;
+import org.serialthreads.context.SerialThreadManager;
+import org.serialthreads.context.SimpleSerialThreadManager;
+
+import java.lang.reflect.Field;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Test runnable for transformer integration tests.
+ */
+public class TestInterruptible implements IRunnable
+{
+  public boolean z;
+  public char c;
+  public byte b;
+  public short s;
+  public int i;
+  public long j;
+  public float f;
+  public double d;
+
+  public long test;
+
+  /**
+   * Execute transformed runnable via serial thread manager.
+   */
+  public void runTransformed()
+  {
+    SimpleSerialThreadManager manager = new SimpleSerialThreadManager(this);
+    SerialThreadManager.registerManager(manager);
+    manager.execute();
+  }
+
+  /**
+   * Execute runnable.
+   */
+  @Interruptible
+  public void run()
+  {
+    long l1 = testLong(true, (char) 1, (byte) 3, (short) 5, 7, 11L);
+    double d1 = testDouble(13.0F, 17.0D);
+    int i1 = testStatic(19);
+
+    test = i1 + l1 + (long) d1;
+
+    checkResult();
+  }
+
+  @Interruptible
+  public long testLong(boolean z, char c, byte b, short s, int i, long j)
+  {
+    z = !z;
+    SerialThreadManager.interrupt();
+    this.z = z;
+    c++;
+    SerialThreadManager.interrupt();
+    this.c = c;
+    b++;
+    SerialThreadManager.interrupt();
+    this.b = b;
+    s++;
+    SerialThreadManager.interrupt();
+    this.s = s;
+    i++;
+    SerialThreadManager.interrupt();
+    this.i = i;
+    j++;
+    SerialThreadManager.interrupt();
+    this.j = j;
+
+    return j;
+  }
+
+  @Interruptible
+  public double testDouble(float f, double d)
+  {
+    f++;
+    SerialThreadManager.interrupt();
+    this.f = f;
+    d++;
+    SerialThreadManager.interrupt();
+    this.d = d;
+
+    return d;
+  }
+
+  @Interruptible
+  public static int testStatic(int i)
+  {
+    i++;
+    SerialThreadManager.interrupt();
+    return i;
+  }
+
+  //
+  // Check results of execution
+  //
+
+  private void checkResult()
+  {
+    assertEquals(false, z); // !true
+    assertEquals((char) 2, c); // 1++
+    assertEquals((byte) 4, b); // 3++
+    assertEquals((short) 6, s); // 5++
+    assertEquals(8, i); // 7++
+    assertEquals(12L, j); // 11++
+
+    assertEquals(14.0F, f, 0.1); // 13++
+    assertEquals(18.0D, d, 0.1); // 17++
+
+    assertEquals(50L, test); // 11++ + 17++ + 19++
+  }
+}
--- src/test/java/org/serialthreads/transformer/TransformerIntegration_AbstractTest.java
+++ src/test/java/org/serialthreads/transformer/TransformerIntegration_AbstractTest.java
@@ -33,16 +33,8 @@ public abstract class TransformerIntegration_AbstractTest
   @Test
   public void testTransform() throws Exception
   {
-    TransformingClassLoader cl = new TransformingClassLoader(strategy);
-
-    Class<? extends IRunnable> clazz = (Class<? extends IRunnable>)
-      cl.loadClass(TestInterruptible.class.getName());
-    IRunnable test = clazz.newInstance();
-    SimpleSerialThreadManager manager = new SimpleSerialThreadManager(test);
-    SerialThreadManager.registerManager(manager);
-    manager.execute();
-
-    checkResult(test);
+    Class<?> clazz = new TransformingClassLoader(strategy).loadClass(TransformerIntegration_AbstractTest.class.getPackage().getName() + ".TestInterruptible");
+    clazz.getMethod("runTransformed").invoke(clazz.newInstance());
   }
 
   /**
@@ -54,102 +46,7 @@ public abstract class TransformerIntegration_AbstractTest
     // disable debug mode to not throw an exception in SerialThreadManager.interrupt()
     SerialThreadManager.DEBUG = false;
 
-    Class<? extends IRunnable> clazz = (Class<? extends IRunnable>)
-      getClass().getClassLoader().loadClass(TestInterruptible.class.getName());
-    IRunnable test = clazz.newInstance();
-    test.run();
-
-    checkResult(test);
-  }
-
-  private void checkResult(IRunnable test) throws Exception
-  {
-    assertFieldEquals(false, test, "z"); // !true
-    assertFieldEquals((char) 2, test, "c"); // 1++
-    assertFieldEquals((byte) 4, test, "b"); // 3++
-    assertFieldEquals((short) 6, test, "s"); // 5++
-    assertFieldEquals(8, test, "i"); // 7++
-    assertFieldEquals(12L, test, "j"); // 11++
-
-    assertFieldEquals(14.0F, test, "f"); // 13++
-    assertFieldEquals(18.0D, test, "d"); // 17++
-
-    assertFieldEquals(50L, test, "test"); // 11++ + 17++ + 19++
-  }
-
-  private void assertFieldEquals(Object expected, IRunnable test, String name) throws Exception
-  {
-    Field field = test.getClass().getField(name);
-    assertEquals(expected, field.get(test));
-  }
-
-  public static class TestInterruptible implements IRunnable
-  {
-    public boolean z;
-    public char c;
-    public byte b;
-    public short s;
-    public int i;
-    public long j;
-    public float f;
-    public double d;
-
-    public long test;
-
-    @Interruptible
-    public void run()
-    {
-      long l1 = testLong(true, (char) 1, (byte) 3, (short) 5, 7, 11L);
-      double d1 = testDouble(13.0F, 17.0D);
-      int i1 = testStatic(19);
-
-      test = i1 + l1 + (long) d1;
-    }
-
-    @Interruptible
-    public long testLong(boolean z, char c, byte b, short s, int i, long j)
-    {
-      z = !z;
-      SerialThreadManager.interrupt();
-      this.z = z;
-      c++;
-      SerialThreadManager.interrupt();
-      this.c = c;
-      b++;
-      SerialThreadManager.interrupt();
-      this.b = b;
-      s++;
-      SerialThreadManager.interrupt();
-      this.s = s;
-      i++;
-      SerialThreadManager.interrupt();
-      this.i = i;
-      j++;
-      SerialThreadManager.interrupt();
-      this.j = j;
-
-      return j;
-    }
-
-    @Interruptible
-    public double testDouble(float f, double d)
-    {
-      f++;
-      SerialThreadManager.interrupt();
-      this.f = f;
-      d++;
-      SerialThreadManager.interrupt();
-      this.d = d;
-
-      return d;
-    }
-
-    @Interruptible
-    public static int testStatic(int i)
-    {
-      i++;
-      SerialThreadManager.interrupt();
-      return i;
-    }
+    Class<?> clazz = getClass().getClassLoader().loadClass(TransformerIntegration_AbstractTest.class.getPackage().getName() + ".TestInterruptible");
+    clazz.getMethod("run").invoke(clazz.newInstance());
   }
 }
