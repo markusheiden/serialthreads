ac82197a05b611058cc36bfcf06abf215b13ce35
Markus Heiden <markus@markusheiden.de>
2010-06-28 21:51:55 +0000

    mh: added test

---
+++ src/test/java/org/serialthreads/transformer/classcache/MethodInfoTest.java
@@ -0,0 +1,65 @@
+package org.serialthreads.transformer.classcache;
+
+import org.junit.Test;
+import org.objectweb.asm.Type;
+
+import java.util.Collections;
+
+import static junit.framework.Assert.assertEquals;
+import static junit.framework.Assert.assertTrue;
+import static org.junit.Assert.assertNotSame;
+
+/**
+ * Test for {@link MethodInfo}.
+ */
+public class MethodInfoTest
+{
+  @Test
+  public void testDefault()
+  {
+    MethodInfo info = new MethodInfo("name", "desc", Collections.singleton(Type.INT_TYPE.getDescriptor()));
+
+    assertEquals("namedesc", info.getID());
+    assertEquals("name", info.getName());
+    assertEquals("desc", info.getDesc());
+    assertEquals(1, info.getAnnotations().size());
+    assertEquals(Type.INT_TYPE.getDescriptor(), info.getAnnotations().iterator().next());
+  }
+
+  @Test
+  public void testToString()
+  {
+    MethodInfo info = new MethodInfo("name", "desc", Collections.singleton(Type.INT_TYPE.getDescriptor()));
+
+    assertEquals("Method info namedesc", info.toString());
+  }
+
+  @Test
+  public void testHasAnnotation()
+  {
+    MethodInfo info = new MethodInfo("name", "desc", Collections.singleton(Type.INT_TYPE.getDescriptor()));
+
+    assertTrue(info.hasAnnotation(Type.INT_TYPE));
+  }
+
+  @Test(expected = UnsupportedOperationException.class)
+  public void testGetAnnotations_immutable()
+  {
+    MethodInfo info = new MethodInfo("name", "desc", Collections.singleton(Type.INT_TYPE.getDescriptor()));
+
+    info.getAnnotations().add("something");
+  }
+
+  @Test
+  public void testCopy()
+  {
+    MethodInfo info = new MethodInfo("name", "desc", Collections.singleton(Type.INT_TYPE.getDescriptor()));
+    MethodInfo copy = info.copy();
+
+    assertEquals(info.getID(), copy.getID());
+    assertEquals(info.getName(), copy.getName());
+    assertEquals(info.getDesc(), copy.getDesc());
+    assertEquals(info.getAnnotations(), copy.getAnnotations());
+    assertNotSame(info.getAnnotations(), copy.getAnnotations());
+  }
+}
