572bb234de6256758623082db85d99e9ede5ef56
Markus Heiden <markus@markusheiden.de>
2011-05-11 00:33:33 +0200

    Reworked processor to check not annotated methods too

--- src/main/java/org/serialthreads/compiler/InterruptibleProcessor.java
+++ src/main/java/org/serialthreads/compiler/InterruptibleProcessor.java
@@ -10,6 +10,7 @@ import javax.lang.model.SourceVersion;
 import javax.lang.model.element.Element;
 import javax.lang.model.element.ExecutableElement;
 import javax.lang.model.element.TypeElement;
+import javax.lang.model.element.VariableElement;
 import javax.lang.model.type.TypeMirror;
 import javax.lang.model.util.Elements;
 import javax.lang.model.util.Types;
@@ -20,41 +21,64 @@ import java.util.Set;
 /**
  * Checks {@link org.serialthreads.Interruptible} annotations.
  */
-@SupportedAnnotationTypes("org.serialthreads.Interruptible")
+@SupportedAnnotationTypes("*")
 @SupportedSourceVersion(SourceVersion.RELEASE_6)
 public class InterruptibleProcessor extends AbstractProcessor
 {
   @Override
   public boolean process(Set<? extends TypeElement> annotations, RoundEnvironment roundEnv)
   {
-    for (Element element : roundEnv.getElementsAnnotatedWith(Interruptible.class))
+    for (Element root : roundEnv.getRootElements())
     {
-      // It had to be a method, because the targets of @Interruptible can only be methods,
-      // but to be sure we are checking per instanceof here.
-      if (element instanceof ExecutableElement)
+      if (root instanceof TypeElement)
       {
-        check((ExecutableElement) element);
+        check((TypeElement) root);
+      }
+      else
+      {
+        processingEnv.getMessager().printMessage(Kind.WARNING, root.getSimpleName()  + " is not a type", root);
       }
     }
 
-    return true;
+    return false;
+  }
+
+  /**
+   * Check a type that its interruptible methods override interruptible methods, if they overrides anything.
+   *
+   * @param type Type to check
+   */
+  private void check(TypeElement type)
+  {
+    for (Element enclosed : type.getEnclosedElements())
+    {
+      if (enclosed instanceof TypeElement)
+      {
+        check((TypeElement) enclosed);
+      }
+      else if (enclosed instanceof ExecutableElement)
+      {
+        check(type, (ExecutableElement) enclosed);
+      }
+      else if (!(enclosed instanceof VariableElement))
+      {
+        processingEnv.getMessager().printMessage(Kind.WARNING, enclosed.getSimpleName()  + " is neither a method nor a field", enclosed);
+      }
+    }
   }
 
   /**
    * Check that an interruptible method overrides another interruptible method, if it overrides anything.
    *
+   * @param type Class containing the method
    * @param overrider Method to check, whether it overrides something
    */
-  private void check(ExecutableElement overrider)
+  private void check(TypeElement type, ExecutableElement overrider)
   {
     Types types = processingEnv.getTypeUtils();
     Elements elements = processingEnv.getElementUtils();
 
-    TypeElement type = (TypeElement) overrider.getEnclosingElement();
-
-    processingEnv.getMessager().printMessage(Kind.WARNING,
-      type.getQualifiedName() + "#" + overrider.getSimpleName() + " is interruptible", overrider);
-
+    boolean interruptible = overrider.getAnnotation(Interruptible.class) != null;
     for (TypeMirror superType : types.directSupertypes(type.asType()))
     {
       TypeElement overriddenType = (TypeElement) types.asElement(superType);
@@ -62,34 +86,21 @@ public class InterruptibleProcessor extends AbstractProcessor
       {
         if (overridden instanceof ExecutableElement)
         {
-          check(type, overrider, overriddenType, (ExecutableElement) overridden);
-        }
-      }
-    }
-  }
+          if (elements.overrides(overrider, (ExecutableElement) overridden, type))
+          {
+            boolean overriddenInterruptible = overridden.getAnnotation(Interruptible.class) != null;
 
-  /**
-   * Check that an interruptible method overrides another interruptible method, if it overrides anything.
-   *
-   * @param overriderType Class of overriding method
-   * @param overrider Overriding method
-   * @param overriddenType Class of potentially overridden method
-   * @param overridden Potentially overridden method
-   */
-  private void check(TypeElement overriderType, ExecutableElement overrider, TypeElement overriddenType, ExecutableElement overridden)
-  {
-    if (processingEnv.getElementUtils().overrides(overrider, overridden, overriderType))
-    {
-      try
-      {
-        processingEnv.getMessager().printMessage(Kind.WARNING,
-          overriderType.getQualifiedName() + "#" + overrider.getSimpleName() + " overrides " +
-          overriddenType.getQualifiedName() + "#" +  overridden.getSimpleName(),
-          overrider);
-      }
-      catch (Exception e)
-      {
-        processingEnv.getMessager().printMessage(Kind.ERROR, e.getMessage(), overrider);
+            if (interruptible != overriddenInterruptible)
+            {
+              processingEnv.getMessager().printMessage(Kind.WARNING,
+                type.getQualifiedName() + "#" + overrider.getSimpleName() + " overrides " +
+                overriddenType.getQualifiedName() + "#" +  overridden.getSimpleName() + ", " +
+                "but their interruptible status does not match",
+                overrider);
+            }
+            break;
+          }
+        }
       }
     }
   }
