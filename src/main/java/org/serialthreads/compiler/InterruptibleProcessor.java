c62da0f23df828edd54a84e4e458b4559ba63a23
Markus Heiden <markus@markusheiden.de>
2011-05-15 13:28:35 +0200

    Reworked

--- src/main/java/org/serialthreads/compiler/InterruptibleProcessor.java
+++ src/main/java/org/serialthreads/compiler/InterruptibleProcessor.java
@@ -10,13 +10,11 @@ import javax.lang.model.SourceVersion;
 import javax.lang.model.element.Element;
 import javax.lang.model.element.ExecutableElement;
 import javax.lang.model.element.TypeElement;
-import javax.lang.model.element.VariableElement;
 import javax.lang.model.type.TypeMirror;
 import javax.lang.model.util.ElementScanner6;
 import javax.lang.model.util.Elements;
 import javax.lang.model.util.Types;
 import javax.tools.Diagnostic.Kind;
-import java.util.List;
 import java.util.Set;
 
 /**
