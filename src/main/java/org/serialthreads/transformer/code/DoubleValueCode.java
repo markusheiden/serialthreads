67e4c545fec8bf9ab9c623b9c373ecb8e3c3e0c9
Markus Heiden <markus@markusheiden.de>
2011-11-06 18:41:33 +0100

    Fixed capturing of return values for values of size 2

--- src/main/java/org/serialthreads/transformer/code/DoubleValueCode.java
+++ src/main/java/org/serialthreads/transformer/code/DoubleValueCode.java
@@ -2,9 +2,13 @@ package org.serialthreads.transformer.code;
 
 import org.objectweb.asm.Type;
 import org.objectweb.asm.tree.AbstractInsnNode;
+import org.objectweb.asm.tree.FieldInsnNode;
+import org.objectweb.asm.tree.InsnList;
 import org.objectweb.asm.tree.InsnNode;
 import org.objectweb.asm.tree.LdcInsnNode;
+import org.objectweb.asm.tree.VarInsnNode;
 
+import static org.objectweb.asm.Opcodes.ALOAD;
 import static org.objectweb.asm.Opcodes.DALOAD;
 import static org.objectweb.asm.Opcodes.DASTORE;
 import static org.objectweb.asm.Opcodes.DCONST_0;
@@ -12,6 +16,10 @@ import static org.objectweb.asm.Opcodes.DCONST_1;
 import static org.objectweb.asm.Opcodes.DLOAD;
 import static org.objectweb.asm.Opcodes.DRETURN;
 import static org.objectweb.asm.Opcodes.DSTORE;
+import static org.objectweb.asm.Opcodes.DUP_X2;
+import static org.objectweb.asm.Opcodes.POP;
+import static org.objectweb.asm.Opcodes.PUTFIELD;
+import static org.objectweb.asm.Opcodes.RETURN;
 
 /**
  * Code for double values.
@@ -37,6 +45,18 @@ public class DoubleValueCode extends AbstractValueCode
     }
 
     return new LdcInsnNode(d);
+  }
+
+  @Override
+  public InsnList pushReturnValue(int localFrame)
+  {
+    InsnList instructions = new InsnList();
+    instructions.add(new VarInsnNode(ALOAD, localFrame));
+    instructions.add(new InsnNode(DUP_X2));
+    instructions.add(new InsnNode(POP));
+    instructions.add(new FieldInsnNode(PUTFIELD, FRAME_IMPL_NAME, "return" + methodName, baseType.getDescriptor()));
+    instructions.add(new InsnNode(RETURN));
+    return instructions;
   }
 
   public boolean isResponsibleFor(Type type)
--- src/main/java/org/serialthreads/transformer/code/LongValueCode.java
+++ src/main/java/org/serialthreads/transformer/code/LongValueCode.java
@@ -2,9 +2,14 @@ package org.serialthreads.transformer.code;
 
 import org.objectweb.asm.Type;
 import org.objectweb.asm.tree.AbstractInsnNode;
+import org.objectweb.asm.tree.FieldInsnNode;
+import org.objectweb.asm.tree.InsnList;
 import org.objectweb.asm.tree.InsnNode;
 import org.objectweb.asm.tree.LdcInsnNode;
+import org.objectweb.asm.tree.VarInsnNode;
 
+import static org.objectweb.asm.Opcodes.ALOAD;
+import static org.objectweb.asm.Opcodes.DUP_X2;
 import static org.objectweb.asm.Opcodes.LALOAD;
 import static org.objectweb.asm.Opcodes.LASTORE;
 import static org.objectweb.asm.Opcodes.LCONST_0;
@@ -12,6 +17,9 @@ import static org.objectweb.asm.Opcodes.LCONST_1;
 import static org.objectweb.asm.Opcodes.LLOAD;
 import static org.objectweb.asm.Opcodes.LRETURN;
 import static org.objectweb.asm.Opcodes.LSTORE;
+import static org.objectweb.asm.Opcodes.POP;
+import static org.objectweb.asm.Opcodes.PUTFIELD;
+import static org.objectweb.asm.Opcodes.RETURN;
 
 /**
  * Code for long values.
@@ -37,6 +45,18 @@ public class LongValueCode extends AbstractValueCode
     }
 
     return new LdcInsnNode(l);
+  }
+
+  @Override
+  public InsnList pushReturnValue(int localFrame)
+  {
+    InsnList instructions = new InsnList();
+    instructions.add(new VarInsnNode(ALOAD, localFrame));
+    instructions.add(new InsnNode(DUP_X2));
+    instructions.add(new InsnNode(POP));
+    instructions.add(new FieldInsnNode(PUTFIELD, FRAME_IMPL_NAME, "return" + methodName, baseType.getDescriptor()));
+    instructions.add(new InsnNode(RETURN));
+    return instructions;
   }
 
   public boolean isResponsibleFor(Type type)
