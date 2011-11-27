760d5eade846ccb3c81feffef817f24965091ebf
Markus Heiden <markus@markusheiden.de>
2011-11-27 20:16:05 +0100

    Reworked class structure (finally finished)

---
+++ src/main/java/org/serialthreads/transformer/strategies/singleframe/AbstractMethodTransformer.java
@@ -0,0 +1,38 @@
+package org.serialthreads.transformer.strategies.singleframe;
+
+import org.objectweb.asm.tree.ClassNode;
+import org.objectweb.asm.tree.MethodNode;
+import org.objectweb.asm.tree.analysis.AnalyzerException;
+import org.serialthreads.transformer.classcache.IClassInfoCache;
+
+/**
+ * Method transformer for abstract methods.
+ */
+class AbstractMethodTransformer extends MethodTransformer
+{
+  /**
+   * Constructor.
+   *
+   * @param clazz class to transform
+   * @param method method to transform
+   * @param classInfoCache class cache to use
+   */
+  protected AbstractMethodTransformer(ClassNode clazz, MethodNode method, IClassInfoCache classInfoCache)
+  {
+    super(clazz, method, classInfoCache);
+  }
+
+  /**
+   * Transform method.
+   *
+   * @return Transformed method
+   * @exception org.objectweb.asm.tree.analysis.AnalyzerException In case of incorrect byte code of the original method
+   */
+  public MethodNode transform() throws AnalyzerException
+  {
+    // add thread and previousFrame arguments to the original method
+    method.desc = changeDesc(method.desc);
+
+    return method;
+  }
+}
---
+++ src/main/java/org/serialthreads/transformer/strategies/singleframe/ConcreteMethodTransformer.java
@@ -0,0 +1,103 @@
+package org.serialthreads.transformer.strategies.singleframe;
+
+import org.objectweb.asm.tree.ClassNode;
+import org.objectweb.asm.tree.FieldInsnNode;
+import org.objectweb.asm.tree.InsnList;
+import org.objectweb.asm.tree.InsnNode;
+import org.objectweb.asm.tree.JumpInsnNode;
+import org.objectweb.asm.tree.LabelNode;
+import org.objectweb.asm.tree.MethodInsnNode;
+import org.objectweb.asm.tree.MethodNode;
+import org.objectweb.asm.tree.VarInsnNode;
+import org.objectweb.asm.tree.analysis.AnalyzerException;
+import org.objectweb.asm.tree.analysis.Frame;
+import org.serialthreads.transformer.MethodNeedsNoTransformationException;
+import org.serialthreads.transformer.classcache.IClassInfoCache;
+
+import java.util.Map;
+
+import static org.objectweb.asm.Opcodes.ALOAD;
+import static org.objectweb.asm.Opcodes.ASTORE;
+import static org.objectweb.asm.Opcodes.DUP;
+import static org.objectweb.asm.Opcodes.GETFIELD;
+import static org.objectweb.asm.Opcodes.IFNONNULL;
+import static org.objectweb.asm.Opcodes.INVOKEVIRTUAL;
+import static org.objectweb.asm.Opcodes.POP;
+import static org.serialthreads.transformer.code.MethodCode.firstLocal;
+
+/**
+ * Method transformer for concrete methods.
+ */
+@SuppressWarnings({"UnusedDeclaration", "UnusedAssignment", "UnnecessaryLocalVariable"})
+class ConcreteMethodTransformer extends MethodTransformer
+{
+  /**
+   * Constructor.
+   *
+   * @param clazz class to transform
+   * @param method method to transform
+   * @param classInfoCache class cache to use
+   */
+  protected ConcreteMethodTransformer(ClassNode clazz, MethodNode method, IClassInfoCache classInfoCache)
+  {
+    super(clazz, method, classInfoCache);
+  }
+
+  /**
+   * Transform method.
+   *
+   * @return Transformed method
+   * @exception org.objectweb.asm.tree.analysis.AnalyzerException In case of incorrect byte code of the original method
+   * @exception org.serialthreads.transformer.MethodNeedsNoTransformationException In case the method needs no byte code transformation
+   */
+  public MethodNode transform() throws AnalyzerException, MethodNeedsNoTransformationException
+  {
+    Frame[] frames = analyze();
+
+    voidReturns();
+    Map<MethodInsnNode, Integer> methodCalls = interruptibleMethodCalls();
+    insertCaptureCode(frames, methodCalls, false);
+    createRestoreHandlerMethod();
+    addThreadAndFrame(methodCalls.keySet());
+    method.desc = changeDesc(method.desc);
+    fixMaxs();
+
+    return method;
+  }
+
+  /**
+   * Insert frame restoring code at the begin of an interruptible method.
+   */
+  private void createRestoreHandlerMethod()
+  {
+    if (log.isDebugEnabled())
+    {
+      log.debug("    Creating restore handler for method");
+    }
+
+    int local = firstLocal(method);
+    final int localThread = local++; // param thread
+    final int localPreviousFrame = local++; // param previousFrame
+    final int localFrame = local++;
+
+    LabelNode normal = new LabelNode();
+
+    // frame = previousFrame.next
+    InsnList getFrame = new InsnList();
+    getFrame.add(new VarInsnNode(ALOAD, localPreviousFrame));
+    getFrame.add(new FieldInsnNode(GETFIELD, FRAME_IMPL_NAME, "next", FRAME_IMPL_DESC));
+    getFrame.add(new InsnNode(DUP));
+    getFrame.add(new JumpInsnNode(IFNONNULL, normal));
+
+    getFrame.add(new InsnNode(POP));
+    // frame = thread.addFrame(previousFrame);
+    getFrame.add(new VarInsnNode(ALOAD, localThread));
+    getFrame.add(new VarInsnNode(ALOAD, localPreviousFrame));
+    getFrame.add(new MethodInsnNode(INVOKEVIRTUAL, THREAD_IMPL_NAME, "addFrame", "(" + FRAME_IMPL_DESC + ")" + FRAME_IMPL_DESC));
+
+    getFrame.add(normal);
+    getFrame.add(new VarInsnNode(ASTORE, localFrame));
+
+    method.instructions.insertBefore(method.instructions.getFirst(), getFrame);
+  }
+}
