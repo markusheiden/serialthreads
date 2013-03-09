fdea87ae70008ff85bfc32a6cd48b0ea9561169c
Markus Heiden <markus@markusheiden.de>
2013-03-10 00:46:10 +0100

    Skip frames when reading class, because the frames get recomputed anyway

--- src/main/java/org/serialthreads/agent/Agent.java
+++ src/main/java/org/serialthreads/agent/Agent.java
@@ -4,11 +4,7 @@ import org.apache.log4j.Logger;
 import org.objectweb.asm.ClassReader;
 import org.objectweb.asm.ClassVisitor;
 import org.objectweb.asm.ClassWriter;
-import org.serialthreads.transformer.IStrategy;
-import org.serialthreads.transformer.ITransformer;
-import org.serialthreads.transformer.LoadUntransformedException;
-import org.serialthreads.transformer.NotTransformableException;
-import org.serialthreads.transformer.Strategies;
+import org.serialthreads.transformer.*;
 import org.serialthreads.transformer.classcache.ClassInfoCacheReflection;
 
 import java.lang.instrument.ClassFileTransformer;
@@ -16,6 +12,7 @@ import java.lang.instrument.IllegalClassFormatException;
 import java.lang.instrument.Instrumentation;
 import java.security.ProtectionDomain;
 
+import static org.objectweb.asm.ClassReader.SKIP_FRAMES;
 import static org.objectweb.asm.ClassWriter.COMPUTE_FRAMES;
 import static org.objectweb.asm.ClassWriter.COMPUTE_MAXS;
 
@@ -25,8 +22,7 @@ import static org.objectweb.asm.ClassWriter.COMPUTE_MAXS;
  * <p/>
  * TODO mh: Do not throw exception in case of transforming problems, because otherwise the vm disables the transformer
  */
-public class Agent implements ClassFileTransformer
-{
+public class Agent implements ClassFileTransformer {
   private static final Logger _logger = Logger.getLogger(Agent.class);
 
   private final ClassInfoCacheReflection _classInfoCache;
@@ -37,61 +33,48 @@ public class Agent implements ClassFileTransformer
    *
    * @param strategy strategy to use
    */
-  public Agent(IStrategy strategy)
-  {
+  public Agent(IStrategy strategy) {
     _classInfoCache = new ClassInfoCacheReflection();
     _transformer = strategy.getTransformer(_classInfoCache);
   }
 
   @Override
-  public byte[] transform(ClassLoader loader, String className, Class<?> classBeingRedefined, ProtectionDomain protectionDomain, byte[] classfileBuffer) throws IllegalClassFormatException
-  {
-    if (loader == null)
-    {
+  public byte[] transform(ClassLoader loader, String className, Class<?> classBeingRedefined, ProtectionDomain protectionDomain, byte[] classfileBuffer) throws IllegalClassFormatException {
+    if (loader == null) {
       // No need to transform classes loaded by the bootstrap classloader
       return null;
     }
 
     boolean failure = true;
-    try
-    {
-      _logger.info("Transforming class " + className + (classBeingRedefined != null? " (redefining)" : " (initial)"));
+    try {
+      _logger.debug("Transforming class " + className + (classBeingRedefined != null ? " (redefining)" : " (initial)"));
       _classInfoCache.start(loader, className, classfileBuffer);
 
       ClassReader reader = new ClassReader(classfileBuffer);
       ClassWriter writer = new ClassWriter(COMPUTE_MAXS + COMPUTE_FRAMES);
       ClassVisitor visitor = new TransformingVisitor(writer, _transformer);
-      reader.accept(visitor, 0);
+      reader.accept(visitor, SKIP_FRAMES);
       byte[] result = writer.toByteArray();
       failure = false;
 
-      _logger.info("Successfully transformed class " + className + (classBeingRedefined != null? " (redefining)" : " (initial)"));
+      _logger.info("Successfully transformed class " + className + (classBeingRedefined != null ? " (redefining)" : " (initial)"));
       _classInfoCache.stop(className);
 
       return result;
-    }
-    catch (LoadUntransformedException e)
-    {
+    } catch (LoadUntransformedException e) {
       // No need to transform byte code
       failure = false;
       return null;
-    }
-    catch (NotTransformableException e)
-    {
+    } catch (NotTransformableException e) {
       _logger.error("Unable to transform " + className, e);
       failure = false;
       throw e;
-    }
-    catch (RuntimeException | Error e)
-    {
+    } catch (RuntimeException | Error e) {
       _logger.error("Failed to transform " + className, e);
       failure = false;
       throw e;
-    }
-    finally
-    {
-      if (failure)
-      {
+    } finally {
+      if (failure) {
         _logger.fatal("Failed to transform " + className);
       }
     }
@@ -104,8 +87,7 @@ public class Agent implements ClassFileTransformer
    * @param arguments arguments
    * @param inst instrumentation
    */
-  public static void premain(String arguments, Instrumentation inst)
-  {
+  public static void premain(String arguments, Instrumentation inst) {
     agentmain(arguments, inst);
   }
 
@@ -116,14 +98,10 @@ public class Agent implements ClassFileTransformer
    * @param arguments arguments
    * @param inst instrumentation
    */
-  public static void agentmain(String arguments, Instrumentation inst)
-  {
-    try
-    {
+  public static void agentmain(String arguments, Instrumentation inst) {
+    try {
       inst.addTransformer(new Agent(Strategies.DEFAULT));
-    }
-    catch (RuntimeException e)
-    {
+    } catch (RuntimeException e) {
       _logger.fatal("Failed to init agent", e);
     }
   }
--- src/main/java/org/serialthreads/agent/TransformingClassLoader.java
+++ src/main/java/org/serialthreads/agent/TransformingClassLoader.java
@@ -11,11 +11,7 @@ import org.serialthreads.transformer.LoadUntransformedException;
 import org.serialthreads.transformer.NotTransformableException;
 import org.serialthreads.transformer.classcache.ClassInfoCacheASM;
 
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.PrintWriter;
-import java.io.StringWriter;
+import java.io.*;
 
 import static org.objectweb.asm.ClassReader.SKIP_DEBUG;
 import static org.objectweb.asm.ClassReader.SKIP_FRAMES;
@@ -25,8 +21,7 @@ import static org.objectweb.asm.ClassWriter.COMPUTE_MAXS;
 /**
  * ClassLoader which applies a transformer to the loaded classes.
  */
-public class TransformingClassLoader extends ClassLoader
-{
+public class TransformingClassLoader extends ClassLoader {
   private final Logger logger = Logger.getLogger(getClass());
 
   private final String[] classPrefixes;
@@ -38,8 +33,7 @@ public class TransformingClassLoader extends ClassLoader
    * @param strategy transformation strategy
    * @param classPrefixes prefixes of class to transform
    */
-  public TransformingClassLoader(IStrategy strategy, String... classPrefixes)
-  {
+  public TransformingClassLoader(IStrategy strategy, String... classPrefixes) {
     this(Thread.currentThread().getContextClassLoader(), strategy, classPrefixes);
   }
 
@@ -50,8 +44,7 @@ public class TransformingClassLoader extends ClassLoader
    * @param strategy transformation strategy
    * @param classPrefixes prefixes of class to transform
    */
-  public TransformingClassLoader(ClassLoader parent, IStrategy strategy, String... classPrefixes)
-  {
+  public TransformingClassLoader(ClassLoader parent, IStrategy strategy, String... classPrefixes) {
     super(parent);
 
     this.classPrefixes = new String[1 + classPrefixes.length];
@@ -65,49 +58,37 @@ public class TransformingClassLoader extends ClassLoader
    * <p/>
    * Tries to transform class first, if possible.
    */
-  protected synchronized Class<?> loadClass(String name, boolean resolve) throws ClassNotFoundException
-  {
+  protected synchronized Class<?> loadClass(String name, boolean resolve) throws ClassNotFoundException {
     Class<?> result = findLoadedClass(name);
-    if (result == null)
-    {
+    if (result == null) {
       byte[] byteCode = null;
 
-      try
-      {
-        if (!shouldBeTransformed(name))
-        {
+      try {
+        if (!shouldBeTransformed(name)) {
           // use default behaviour for all classes not directly marked as interruptible
           return super.loadClass(name, resolve);
         }
 
         byteCode = loadByteCode(name);
-        if (byteCode == null)
-        {
+        if (byteCode == null) {
           // use default behaviour for all classes that could not be loaded directly
           return super.loadClass(name, resolve);
         }
 
         byteCode = transform(byteCode);
 //        FileCopyUtils.copy(byteCode, new File("./" + name + ".class"));
-      }
-      catch (LoadUntransformedException e)
-      {
+      } catch (LoadUntransformedException e) {
         // load class, because class should use this classloader to load referenced classes
-      }
-      catch (NotTransformableException e)
-      {
+      } catch (NotTransformableException e) {
         throw new ClassNotFoundException("Class " + name + " has transformation errors", e);
-      }
-      catch (IOException e)
-      {
+      } catch (IOException e) {
         throw new ClassNotFoundException("Class file for " + name + " could not be read", e);
       }
 
       result = defineClass(name, byteCode, 0, byteCode.length);
     }
 
-    if (resolve)
-    {
+    if (resolve) {
       resolveClass(result);
     }
 
@@ -121,12 +102,10 @@ public class TransformingClassLoader extends ClassLoader
    * @return byte code or null, if class file does not exist
    * @exception IOException in case of IO exception while reading the class file
    */
-  private byte[] loadByteCode(String name) throws IOException
-  {
+  private byte[] loadByteCode(String name) throws IOException {
     // class content
     InputStream in = getResourceAsStream(name.replace('.', '/') + ".class");
-    if (in == null)
-    {
+    if (in == null) {
       return null;
     }
 
@@ -134,32 +113,22 @@ public class TransformingClassLoader extends ClassLoader
     ByteArrayOutputStream out = new ByteArrayOutputStream();
 
     // try to load the byte code
-    try
-    {
+    try {
       byte[] buffer = new byte[16384];
-      for (int bytesRead; (bytesRead = in.read(buffer)) != -1; )
-      {
+      for (int bytesRead; (bytesRead = in.read(buffer)) != -1; ) {
         out.write(buffer, 0, bytesRead);
       }
       out.flush();
       return out.toByteArray();
-    }
-    finally
-    {
-      try
-      {
+    } finally {
+      try {
         in.close();
-      }
-      catch (IOException e)
-      {
+      } catch (IOException e) {
         logger.warn("Unable to close InputStream", e);
       }
-      try
-      {
+      try {
         out.close();
-      }
-      catch (IOException e)
-      {
+      } catch (IOException e) {
         logger.warn("Unable to close OutputStream", e);
       }
     }
@@ -171,17 +140,15 @@ public class TransformingClassLoader extends ClassLoader
    * @param byteCode byte code of class
    * @return modified byte code of class
    */
-  protected byte[] transform(byte[] byteCode) throws NotTransformableException
-  {
+  protected byte[] transform(byte[] byteCode) throws NotTransformableException {
     ClassReader reader = new ClassReader(byteCode);
     ClassWriter writer = new ClassWriter(COMPUTE_MAXS + COMPUTE_FRAMES);
     ClassVisitor visitor = new TransformingVisitor(createVisitor(writer), transformer);
 
-    reader.accept(visitor, 0);
+    reader.accept(visitor, SKIP_FRAMES);
     byte[] result = writer.toByteArray();
 
-    if (logger.isDebugEnabled())
-    {
+    if (logger.isDebugEnabled()) {
       StringWriter output = new StringWriter(4096);
       new ClassReader(result).accept(new TraceClassVisitor(new PrintWriter(output)), SKIP_FRAMES + SKIP_DEBUG);
       logger.debug("Byte code:\n" + output);
@@ -195,12 +162,9 @@ public class TransformingClassLoader extends ClassLoader
    *
    * @param className fully qualified name of class
    */
-  protected boolean shouldBeTransformed(String className)
-  {
-    for (String classPrefix : classPrefixes)
-    {
-      if (className.startsWith(classPrefix))
-      {
+  protected boolean shouldBeTransformed(String className) {
+    for (String classPrefix : classPrefixes) {
+      if (className.startsWith(classPrefix)) {
         return true;
       }
     }
@@ -213,8 +177,7 @@ public class TransformingClassLoader extends ClassLoader
    *
    * @param writer byte code writer
    */
-  protected ClassVisitor createVisitor(ClassWriter writer)
-  {
+  protected ClassVisitor createVisitor(ClassWriter writer) {
     return writer;
   }
 }
