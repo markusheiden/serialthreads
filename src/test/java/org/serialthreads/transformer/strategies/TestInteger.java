d81602ba9311b4b2ce250f67dad1c6f9d9468918
Markus Heiden <markus@markusheiden.de>
2013-11-25 22:44:17 +0100

    Added integration tests

---
+++ src/test/java/org/serialthreads/transformer/strategies/TestInteger.java
@@ -0,0 +1,55 @@
+package org.serialthreads.transformer.strategies;
+
+import org.serialthreads.Interrupt;
+import org.serialthreads.Interruptible;
+import org.serialthreads.context.IRunnable;
+
+/**
+ * Object against which the tests run.
+ */
+public class TestInteger implements IRunnable {
+  public int value0 = -1;
+  public int value1 = -1;
+  public int value2 = -1;
+  public int value3 = -1;
+  public int value4 = -1;
+  public int value5 = -1;
+  public int value6 = -1;
+  public int value7 = -1;
+  public int value8 = -1;
+  public int value9 = -1;
+
+  @Interruptible
+  public void run() {
+    int local0 = 0;
+    int local1 = local0 + 1;
+    int local2 = local1 + 1;
+    int local3 = local2 + 1;
+    int local4 = local3 + 1;
+    int local5 = local4 + 1;
+    int local6 = local5 + 1;
+    int local7 = local6 + 1;
+    int local8 = local7 + 1;
+    int local9 = local8 + 1;
+
+    interrupt();
+
+    value0 = local0;
+    value1 = local1;
+    value2 = local2;
+    value3 = local3;
+    value4 = local4;
+    value5 = local5;
+    value6 = local6;
+    value7 = local7;
+    value8 = local8;
+    value9 = local9;
+
+    interrupt();
+  }
+
+  @Interrupt
+  private void interrupt() {
+    // method call will be redirected to interrupt code
+  }
+}
--- src/test/java/org/serialthreads/transformer/strategies/TransformerIntegration_AbstractTest.java
+++ src/test/java/org/serialthreads/transformer/strategies/TransformerIntegration_AbstractTest.java
@@ -7,10 +7,13 @@ import org.objectweb.asm.ClassVisitor;
 import org.objectweb.asm.ClassWriter;
 import org.objectweb.asm.util.TraceClassVisitor;
 import org.serialthreads.agent.TransformingClassLoader;
+import org.serialthreads.context.IRunnable;
 import org.serialthreads.context.SerialThreadManager;
 import org.serialthreads.transformer.IStrategy;
 
 import java.io.PrintWriter;
+
+import static org.junit.Assert.assertEquals;
 
 /**
  * Integration test for transformer.
@@ -21,10 +24,19 @@ public abstract class TransformerIntegration_AbstractTest {
    */
   protected IStrategy strategy;
 
+  private TransformingClassLoader classLoader;
+
   @Before
   @After
   public void setUp() {
     SerialThreadManager.DEBUG = true;
+
+    classLoader = new TransformingClassLoader(strategy) {
+      @Override
+      protected ClassVisitor createVisitor(ClassWriter writer) {
+        return new TraceClassVisitor(writer, new PrintWriter(System.out));
+      }
+    };
   }
 
   /**
@@ -32,12 +44,7 @@ public abstract class TransformerIntegration_AbstractTest {
    */
   @Test
   public void testTransform() throws Exception {
-    Class<?> clazz = new TransformingClassLoader(strategy) {
-      @Override
-      protected ClassVisitor createVisitor(ClassWriter writer) {
-        return new TraceClassVisitor(writer, new PrintWriter(System.out));
-      }
-    }.loadClass(TestInterruptible.class.getName());
+    Class<?> clazz = classLoader.loadClass(TestInterruptible.class.getName());
     clazz.getMethod("runTransformed").invoke(clazz.newInstance());
   }
 
@@ -51,5 +58,53 @@ public abstract class TransformerIntegration_AbstractTest {
 
     Class<?> clazz = getClass().getClassLoader().loadClass(TestInterruptible.class.getName());
     clazz.getMethod("run").invoke(clazz.newInstance());
+  }
+
+  @Test
+  public void testLocalStorage_Integer() throws Exception {
+    create(TestInteger.class);
+    run();
+    for (int i = 0; i < 9; i++) {
+      assertEquals(-1, field("value" + i));
+    }
+    run();
+    for (int i = 0; i < 9; i++) {
+      assertEquals(i, field("value" + i));
+    }
+  }
+
+  //
+  // Reflection test support
+  //
+
+  /**
+   * Test object.
+   */
+  private Object test;
+
+  /**
+   * Create and transform test object.
+   *
+   * @param clazz Clazz of test object
+   */
+  private void create(Class<? extends IRunnable> clazz) throws Exception {
+    test = classLoader.loadClass(clazz.getName()).newInstance();
+  }
+
+  /**
+   * Run test object once.
+   */
+  private void run() throws Exception {
+    test.getClass().getMethod("run").invoke(test);
+  }
+
+  /**
+   * Get value of a field of the test object.
+   *
+   * @param name Name of the field
+   * @return Value of the field
+   */
+  private Object field(String name) throws Exception {
+    return test.getClass().getField(name).get(test);
   }
 }
