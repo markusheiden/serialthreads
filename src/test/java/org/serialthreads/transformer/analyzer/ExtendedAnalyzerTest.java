49660c480e47054afd994adeb365d27f38bbaa0a
Markus Heiden <markus@markusheiden.de>
2013-12-01 12:38:35 +0100

    Added simple test for backflow analysis

--- src/test/java/org/serialthreads/transformer/analyzer/ExtendedAnalyzerTest.java
+++ src/test/java/org/serialthreads/transformer/analyzer/ExtendedAnalyzerTest.java
@@ -2,13 +2,21 @@ package org.serialthreads.transformer.analyzer;
 
 import org.junit.Test;
 import org.objectweb.asm.Type;
+import org.objectweb.asm.tree.InsnList;
+import org.objectweb.asm.tree.InsnNode;
+import org.objectweb.asm.tree.MethodNode;
+import org.objectweb.asm.tree.VarInsnNode;
 import org.objectweb.asm.tree.analysis.BasicValue;
+import org.objectweb.asm.tree.analysis.Frame;
+
+import java.util.Collections;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.fail;
+import static org.objectweb.asm.Opcodes.*;
 
 /**
- * Test for ExtendedAnalyzer.
+ * Test for {@link ExtendedAnalyzer}.
  */
 public class ExtendedAnalyzerTest {
   @Test
@@ -63,5 +71,31 @@ public class ExtendedAnalyzerTest {
     } catch (IndexOutOfBoundsException e) {
       // expected
     }
+  }
+
+  @Test
+  public void testBackflow_simple() throws Exception {
+    MethodNode method = new MethodNode(0, "test", "()I", null, new String[0]);
+    method.maxLocals = 3;
+    method.maxStack = 1;
+
+    InsnList instructions = method.instructions;
+    // 0: local1 = 1
+    instructions.add(new InsnNode(ICONST_1));
+    instructions.add(new VarInsnNode(ISTORE, 1));
+    // 2: local2 = 1
+    instructions.add(new InsnNode(ICONST_1));
+    instructions.add(new VarInsnNode(ISTORE, 2));
+
+    // 4: return local1 -> a this point just local1 is needed for the remaining code
+    instructions.add(new VarInsnNode(ILOAD, 1));
+    instructions.add(new InsnNode(IRETURN));
+
+    ExtendedAnalyzer analyzer = new ExtendedAnalyzer(null, null, null, null, false);
+    Frame[] frames = analyzer.analyze("test", method);
+
+    // Check that at instruction 4 just local 1 is declared as needed for the remaining code
+    ExtendedFrame frame4 = (ExtendedFrame) frames[4];
+    assertEquals(Collections.singleton(1), frame4.neededLocals);
   }
 }
