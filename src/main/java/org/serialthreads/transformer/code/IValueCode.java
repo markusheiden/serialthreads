0b691afae568b7b4be9f786a234a13c2a38ea775
Markus Heiden <markus.heiden@s24.com>
2015-10-12 00:45:36 +0200

    Format code

--- src/main/java/org/serialthreads/transformer/code/IValueCode.java
+++ src/main/java/org/serialthreads/transformer/code/IValueCode.java
@@ -9,147 +9,168 @@ import org.objectweb.asm.tree.VarInsnNode;
  * Value specific code generation.
  */
 public interface IValueCode {
-  /**
-   * Generate code to get the array for locals of the frame.
-   *
-   * @param localFrame local with frame
-   */
-  public InsnList getLocals(int localFrame);
+   /**
+    * Generate code to get the array for locals of the frame.
+    *
+    * @param localFrame
+    *           local with frame.
+    */
+   InsnList getLocals(int localFrame);
 
-  /**
-   * Generate code to capture a local variable to a frame.
-   * The frame is expected to be already on the top of the stack.
-   *
-   * @param local number of local variable
-   * @param index index of local among locals of the same type
-   * @param localFrame frame to push to
-   */
-  public InsnList pushLocalVariableFast(int local, int index, int localFrame);
+   /**
+    * Generate code to capture a local variable to a frame. The frame is expected to be already on the top of the stack.
+    *
+    * @param local
+    *           number of local variable.
+    * @param index
+    *           index of local among locals of the same type.
+    * @param localFrame
+    *           frame to push to.
+    */
+   InsnList pushLocalVariableFast(int local, int index, int localFrame);
 
-  /**
-   * Generate code to capture a local variable to a frame.
-   * The frame is expected to be already on the top of the stack.
-   *
-   * @param local number of local variable
-   * @param index index of local among locals of the same type
-   */
-  public InsnList pushLocalVariable(int local, int index);
+   /**
+    * Generate code to capture a local variable to a frame. The frame is expected to be already on the top of the stack.
+    *
+    * @param local
+    *           number of local variable.
+    * @param index
+    *           index of local among locals of the same type.
+    */
+   InsnList pushLocalVariable(int local, int index);
 
-  /**
-   * Generate code to capture the current stack as a return value into the thread.
-   *
-   * @param localThread Thread to push to
-   */
-  public InsnList pushReturnValue(int localThread);
+   /**
+    * Generate code to capture the current stack as a return value into the thread.
+    *
+    * @param localThread
+    *           Thread to push to.
+    */
+   InsnList pushReturnValue(int localThread);
 
-  /**
-   * Generate code to restore a local variable from a frame.
-   * The frame is expected to be already on the top of the stack.
-   *
-   * @param local number of local variable
-   * @param index index of local among locals of the same type
-   * @param localFrame frame to push to
-   */
-  public InsnList popLocalVariableFast(int local, int index, int localFrame);
+   /**
+    * Generate code to restore a local variable from a frame. The frame is expected to be already on the top of the
+    * stack.
+    *
+    * @param local
+    *           number of local variable.
+    * @param index
+    *           index of local among locals of the same type.
+    * @param localFrame
+    *           frame to push to.
+    */
+   InsnList popLocalVariableFast(int local, int index, int localFrame);
 
-  /**
-   * Generate code to restore a local variable from a frame.
-   * The frame is expected to be already on the top of the stack.
-   *
-   * @param local number of local variable
-   * @param index index of local among locals of the same type
-   */
-  public InsnList popLocalVariable(int local, int index);
+   /**
+    * Generate code to restore a local variable from a frame. The frame is expected to be already on the top of the
+    * stack.
+    *
+    * @param local
+    *           number of local variable.
+    * @param index
+    *           index of local among locals of the same type.
+    */
+   InsnList popLocalVariable(int local, int index);
 
-  /**
-   * Generate code to restore the return value from a frame.
-   *
-   * @param localFrame frame to pop from
-   */
-  public InsnList popReturnValue(int localFrame);
+   /**
+    * Generate code to restore the return value from a frame.
+    *
+    * @param localFrame
+    *           frame to pop from.
+    */
+   InsnList popReturnValue(int localFrame);
 
-  /**
-   * Generate code to get the array for stack elements of the frame.
-   *
-   * @param localFrame local with frame
-   */
-  public InsnList getStacks(int localFrame);
+   /**
+    * Generate code to get the array for stack elements of the frame.
+    *
+    * @param localFrame
+    *           local with frame.
+    */
+   InsnList getStacks(int localFrame);
 
-  /**
-   * Generate code to capture the top stack element to a frame.
-   * The frame is expected to be already on the top of the stack.
-   * The stack element to push is expected to be the element under the frame.
-   *
-   * @param index index of stack element among stack elements of the same type
-   * @param localFrame frame to push to
-   */
-  public InsnList pushStack(int index, int localFrame);
+   /**
+    * Generate code to capture the top stack element to a frame. The frame is expected to be already on the top of the
+    * stack. The stack element to push is expected to be the element under the frame.
+    *
+    * @param index
+    *           index of stack element among stack elements of the same type.
+    * @param localFrame
+    *           frame to push to.
+    */
+   InsnList pushStack(int index, int localFrame);
 
-  /**
-   * Generate code to restore a stack element from a frame.
-   * The frame is expected to be already on the top of the stack.
-   *
-   * @param index index of stack element among stack elements of the same type
-   * @param localFrame frame to pop from
-   */
-  public InsnList popStack(int index, int localFrame);
+   /**
+    * Generate code to restore a stack element from a frame. The frame is expected to be already on the top of the
+    * stack.
+    *
+    * @param index
+    *           index of stack element among stack elements of the same type.
+    * @param localFrame
+    *           frame to pop from.
+    */
+   InsnList popStack(int index, int localFrame);
 
-  /**
-   * Push value from local onto stack.
-   *
-   * @param i number of local variable
-   */
-  public VarInsnNode load(int i);
+   /**
+    * Push value from local onto stack.
+    *
+    * @param i
+    *           number of local variable.
+    */
+   VarInsnNode load(int i);
 
-  /**
-   * Pop value from stack into local.
-   *
-   * @param i number of local variable
-   */
-  public VarInsnNode store(int i);
+   /**
+    * Pop value from stack into local.
+    *
+    * @param i
+    *           number of local variable.
+    */
+   VarInsnNode store(int i);
 
-  /**
-   * Copy a local into another local.
-   *
-   * @param from Number of local variable to copy from
-   * @param to Number of local variable to copy to
-   */
-  public InsnList move(int from, int to);
+   /**
+    * Copy a local into another local.
+    *
+    * @param from
+    *           Number of local variable to copy from.
+    * @param to
+    *           Number of local variable to copy to.
+    */
+   InsnList move(int from, int to);
 
-  /**
-   * Pop value from stack.
-   */
-  public AbstractInsnNode pop();
+   /**
+    * Pop value from stack.
+    */
+   AbstractInsnNode pop();
 
-  /**
-   * Push null value on stack.
-   */
-  public AbstractInsnNode pushNull();
+   /**
+    * Push null value on stack.
+    */
+   AbstractInsnNode pushNull();
 
-  /**
-   * Push value on stack.
-   *
-   * @param value value
-   */
-  public AbstractInsnNode push(Object value);
+   /**
+    * Push value on stack.
+    *
+    * @param value
+    *           value.
+    */
+   AbstractInsnNode push(Object value);
 
-  /**
-   * Return statement.
-   * Returns null value (if not void).
-   */
-  public InsnList returnNull();
+   /**
+    * Return statement. Returns null value (if not void).
+    */
+   InsnList returnNull();
 
-  /**
-   * Is this type compatible with another type?
-   *
-   * @param type type
-   */
-  public boolean isCompatibleWith(Type type);
+   /**
+    * Is this type compatible with another type?
+    *
+    * @param type
+    *           type.
+    */
+   boolean isCompatibleWith(Type type);
 
-  /**
-   * Is this code responsible for handling the given type?
-   *
-   * @param type type
-   */
-  public boolean isResponsibleFor(Type type);
+   /**
+    * Is this code responsible for handling the given type?
+    *
+    * @param type
+    *           type.
+    */
+   boolean isResponsibleFor(Type type);
 }
