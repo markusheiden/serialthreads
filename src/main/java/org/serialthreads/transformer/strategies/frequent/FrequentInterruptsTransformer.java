0003948421733643e4043d5c85b47843d89afd8e
Markus Heiden <markus.heiden@s24.com>
2018-02-13 09:28:54 +0100

    Add TODO

--- src/main/java/org/serialthreads/transformer/strategies/frequent/FrequentInterruptsTransformer.java
+++ src/main/java/org/serialthreads/transformer/strategies/frequent/FrequentInterruptsTransformer.java
@@ -73,6 +73,7 @@ public class FrequentInterruptsTransformer extends AbstractTransformer {
 
     // TODO 2008-09-23 mh: just for classes which contain at least one not static, interruptible method?
     // TODO 2008-09-25 mh: make protected and do not create, when a subclass already has this field?
+    // TODO 2018-02-13 mh: use this field.
     clazz.fields.add(threadCode.threadField());
   }
 }
