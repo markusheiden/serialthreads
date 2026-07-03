492fe04cdf2f01991ca18186e9e8c1448aa659dc
Markus Heiden <markus@markusheiden.de>
2026-07-03 17:08:12 +0200

    Bump dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,7 +17,7 @@ repositories {
 java {
     // https://docs.gradle.org/current/userguide/toolchains.html
     toolchain {
-        languageVersion = JavaLanguageVersion.of(25)
+        languageVersion = JavaLanguageVersion.of(26)
         // Use Eclipse Temurin (provided by Adoptium).
         vendor = JvmVendorSpec.ADOPTIUM
     }
@@ -33,21 +33,21 @@ configurations.all {
 dependencies {
     implementation(platform(libs.spring.boot.bom))
 
-    implementation(libs.slf4j.api)
+    implementation("org.slf4j:slf4j-api")
 
     implementation(libs.asm)
     implementation(libs.asm.analysis)
     implementation(libs.asm.tree)
     implementation(libs.asm.util)
 
-    testImplementation(libs.junit.platform.launcher)
-    testImplementation(libs.junit.jupiter)
-    testImplementation(libs.assertj.core)
-    testRuntimeOnly(libs.logback.classic)
+    testImplementation("org.junit.platform:junit-platform-launcher")
+    testImplementation("org.junit.jupiter:junit-jupiter")
+    testImplementation("org.assertj:assertj-core")
+    testRuntimeOnly("ch.qos.logback:logback-classic")
     testImplementation(testFixtures(project(":")))
 
     testFixturesImplementation(platform(libs.spring.boot.bom))
-    testFixturesImplementation(libs.junit.jupiter)
+    testFixturesImplementation("org.junit.jupiter:junit-jupiter")
 }
 
 tasks.withType<AbstractArchiveTask> {
